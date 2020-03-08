948eaab547690ab5f0bdd155776e89b14a7694f0
Ryo Yamashita <qryxip@gmail.com>
2020-03-08 12:33:30 +0900

    Set `low_speed_limit` and `low_speed_time`

--- download/src/lib.rs
+++ download/src/lib.rs
@@ -3,13 +3,17 @@ use curl::easy::Easy;
 use std::time::{Duration, Instant};
 
 static URL: &str = "https://file-examples.com/wp-content/uploads/2017/02/zip_2MB.zip";
-const TIMEOUT: Duration = Duration::from_secs(0);
+const TIMEOUT: Duration = Duration::from_secs(20);
+const LOW_SPEED_LIMIT: u32 = 1;
+const LOW_SPEED_TIME: Duration = Duration::from_secs(30);
 
 pub fn download() -> Result<(), curl::Error> {
     let mut len = 0;
     let mut handle = Easy::new();
     handle.url(URL)?;
     handle.timeout(TIMEOUT)?;
+    handle.low_speed_limit(LOW_SPEED_LIMIT)?;
+    handle.low_speed_time(LOW_SPEED_TIME)?;
     let mut transfer = handle.transfer();
     transfer.write_function(|data| {
         len += data.len();
@@ -22,7 +26,7 @@ pub fn download() -> Result<(), curl::Error> {
     let time = Instant::now() - start;
     drop(transfer);
 
-    assert_eq!(len, 2 * 1024usize.pow(2));
+    assert_eq!(len, 2_097_152);
 
     println!("It took {:?}", time);
     Ok(())
