ddef6ccc5f203433e0a7e04450ba7cd8606dbaa6
Ryo Yamashita <qryxip@gmail.com>
2020-03-08 11:56:29 +0900

    Download 1000 times

--- download/src/lib.rs
+++ download/src/lib.rs
@@ -6,24 +6,28 @@ static URL: &str = "http://localhost:8000/128kib-file";
 const TIMEOUT: Duration = Duration::from_secs(0);
 
 pub fn download() -> Result<(), curl::Error> {
-    let mut content = vec![];
+    let mut time = Duration::new(0, 0);
 
-    let mut handle = Easy::new();
-    handle.url(URL)?;
-    handle.timeout(TIMEOUT)?;
-    let mut transfer = handle.transfer();
-    transfer.write_function(|data| {
-        content.extend_from_slice(data);
-        Ok(data.len())
-    })?;
+    for _ in 0..1000 {
+        let mut content = vec![];
 
-    let start = Instant::now();
-    transfer.perform()?;
-    let time = Instant::now() - start;
-    drop(transfer);
+        let mut handle = Easy::new();
+        handle.url(URL)?;
+        handle.timeout(TIMEOUT)?;
+        let mut transfer = handle.transfer();
+        transfer.write_function(|data| {
+            content.extend_from_slice(data);
+            Ok(data.len())
+        })?;
 
-    assert_eq!(content, vec![0; 128 * 1024]);
-    println!("Ok. Successfully downloaded from {}", URL);
+        let start = Instant::now();
+        transfer.perform()?;
+        time += Instant::now() - start;
+        drop(transfer);
+
+        assert_eq!(content, vec![0; 128 * 1024]);
+    }
+
     println!("It took {:?}", time);
     Ok(())
 }
