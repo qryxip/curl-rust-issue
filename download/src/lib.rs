463dd38f48748a2655a7d7d8ac3196506dd3e5c4
Ryo Yamashita <qryxip@gmail.com>
2020-03-08 12:26:00 +0900

    Use File Examples

--- download/src/lib.rs
+++ download/src/lib.rs
@@ -2,31 +2,27 @@ use curl::easy::Easy;
 
 use std::time::{Duration, Instant};
 
-static URL: &str = "http://localhost:8000/128kib-file";
+static URL: &str = "https://file-examples.com/wp-content/uploads/2017/02/zip_2MB.zip";
 const TIMEOUT: Duration = Duration::from_secs(0);
 
 pub fn download() -> Result<(), curl::Error> {
-    let mut time = Duration::new(0, 0);
+    let mut len = 0;
+    let mut handle = Easy::new();
+    handle.url(URL)?;
+    handle.timeout(TIMEOUT)?;
+    let mut transfer = handle.transfer();
+    transfer.write_function(|data| {
+        len += data.len();
+        Ok(data.len())
+    })?;
+    transfer.perform()?;
 
-    for _ in 0..1000 {
-        let mut content = vec![];
+    let start = Instant::now();
+    transfer.perform()?;
+    let time = Instant::now() - start;
+    drop(transfer);
 
-        let mut handle = Easy::new();
-        handle.url(URL)?;
-        handle.timeout(TIMEOUT)?;
-        let mut transfer = handle.transfer();
-        transfer.write_function(|data| {
-            content.extend_from_slice(data);
-            Ok(data.len())
-        })?;
-
-        let start = Instant::now();
-        transfer.perform()?;
-        time += Instant::now() - start;
-        drop(transfer);
-
-        assert_eq!(content, vec![0; 128 * 1024]);
-    }
+    assert_eq!(len, 2 * 1024usize.pow(2));
 
     println!("It took {:?}", time);
     Ok(())
